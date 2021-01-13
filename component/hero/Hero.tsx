1dd6f317eb96eb6c7252e13e3dada4de6db301d0
Muhammad Tegar Akmal <eategar@gmail.com>
2021-01-13 20:17:57 +0700

    setup button

--- component/hero/Hero.tsx
+++ component/hero/Hero.tsx
@@ -12,8 +12,9 @@ function Hero() {
     return (
         <section className={styles.hero}>
             <div className={styles.header }>
-                <h1 className="text-4xl font-bold text-white sm:text-lg lg:text-6xl md:text-4xl ">YUK BELAJAR NGODING</h1>
+                <h1 className="text-xl font-bold text-white sm:text-lg lg:text-6xl md:text-4xl ">YUK BELAJAR NGODING</h1>
                 <h2 className="w-11/12 mt-8 font-medium leading-7 tracking-wide text-white">Nextcode indonesia  hadir sebagai wadah para developer muda indonesia yang ingin belajar coding seperti pemograman web,machine learning,pemogramana web GRATIS. </h2>
+                <button className="mt-8 btn btn--white">Get Started</button>
             </div>
             {
                 dekstopUp &&
