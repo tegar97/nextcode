899e847efbb75b4b51ac10342dbbad37604ef763
Muhammad Tegar Akmal <eategar@gmail.com>
2021-01-16 23:21:45 +0700

    add artikel

--- component/Artikel/Artikel.tsx
+++ component/Artikel/Artikel.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import ArtikelCard from '../artikel-card/Artikel-card'
 
 const Artikel = () =>{
     return (
-        <section className="mt-20 text-center">
+        <section className="p-20 mt-20 text-center">
             <h1 className="text-4xl">Artikel</h1>
-
+            <div className="grid gap-10 mt-3 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 ">
+               <ArtikelCard/>
+               <ArtikelCard/>
+               <ArtikelCard/>
+               <ArtikelCard/>
+            </div>
         </section>
     )
 }
--- component/artikel-card/Artikel-card.tsx
+++ component/artikel-card/Artikel-card.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
+import styles from './../../styles/components/Artikel-card/ArtikelCard.module.scss'
 function ArtikelCard() {
     return (
-        <div>
-            
+        <div className={`bg-white dark:bg-grey-800 text-left ${styles.card}`}>
+            <div className={styles.artikelImage}>
+                <Image src="/artikel.jpg" alt="Artikel "  width={500} height={500}/>
+            </div>
+            <small>React js</small>
+            <h2>Tutorial Java 21</h2>
+            <div className="flex flex-row mt-10">
+                <p  className="text-sm">11 menit lalu</p>
+                <li className="ml-2 text-sm">5 menit baca</li>
+            </div>
         </div>
     )
 }
