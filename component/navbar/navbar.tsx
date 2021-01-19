f14459a20185088b5f749f907fa537401df41f25
Muhammad Tegar Akmal <eategar@gmail.com>
2021-01-19 17:40:08 +0700

    navbar on scroll

--- component/navbar/navbar.tsx
+++ component/navbar/navbar.tsx
@@ -30,16 +30,19 @@ function Navbar() {
     <nav className={`${styles.navbar } ${active && homeRouter ? 'bg-white dark:bg-gray-900' : ''} ${!homeRouter ? 'bg-white dark:bg-gray-900' : ''} `} style={{top: active && !homeRouter ? '-100px' : ''}}>
             <Link href="/">
                 <div className="flex items-center p-2 cursor-pointer">
-                  <Image src={theme  === "light" && active  || !homeRouter ? '/logo-black.png' : '/logo-white.png'} alt="logo" width={100} height={30} />
+                  <Image src={theme  === "light"  ? '/logo-black.png' : '/logo-white.png'} alt="logo" width={100} height={30} />
 
                 </div>
             </Link>
-            <ul className={`flex flex-row   ${!active && homeRouter ? 'text-white dark:text-white' : 'text-black-50  dark:text-white  '}`}> 
-                <Link href="/path"><li className="mr-3 text-sm cursor-pointer ">TUTORIAL</li></Link>
-                <li className="mr-3 text-sm cursor-pointer ">PROGRAM</li>
-                <li className={`mr-3 text-sm cursor-pointer  } `}>KATEGORI</li>
+            <ul style={{marginRight: active ? '65%' : '',transition: '.5s all'}} className={`flex flex-row   ${!active && homeRouter ? 'text-white dark:text-white' : 'text-black-50  dark:text-white  '}`} > 
+                <Link href="/path"><li className="mr-3 text-sm cursor-pointer ">Artikel</li></Link>
+                <Link href="/path"><li className="mr-3 text-sm cursor-pointer ">Kategori</li></Link>
+                <Link href="/path"><li className="mr-3 text-sm cursor-pointer ">Tutorial Series</li></Link>
+                <Link href="/path"><li className="mr-3 text-sm cursor-pointer ">Tentang</li></Link>
+              
             </ul>
             <div>
+                <button ><i className=" fas fa-search color-black-50 dark:color-white" style={{color: theme === 'light' ? '#000' : '#fff'}} ></i></button>
                 <button ><i className=" fas fa-search color-black-50 dark:color-white" style={{color: theme === 'light' ? '#000' : '#fff'}} ></i></button>
             </div>
     </nav>
